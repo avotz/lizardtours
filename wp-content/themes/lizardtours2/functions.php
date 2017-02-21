488c606c20052c115da4a1cd2664fc43b8e06c76
Alonso Chavarria Cubero <alons182@hotmail.com>
2017-02-21 10:39:42 -0600

    se habilito el gateway de booking

--- wp-content/themes/lizardtours2/functions.php
+++ wp-content/themes/lizardtours2/functions.php
@@ -293,10 +293,10 @@ function woocommerce_support() {
     add_theme_support( 'woocommerce' );
 }
 
-function manage_available_gateways( $gateways ) {
+/*function manage_available_gateways( $gateways ) {
 		unset($gateways['wc-booking-gateway']);
 		return $gateways;
-	}
+	}*/
 
 add_filter( 'woocommerce_available_payment_gateways', 'manage_available_gateways' );
 /**
