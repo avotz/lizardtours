a808fa274132af6470cbe9535e3b19bfa583a9b3
Alonso Avotz <alonso@avotz.com>
2017-05-29 12:34:48 -0600

    fix count tours select ajax

--- wp-content/themes/lizardtours2/assets/js/main.js
+++ wp-content/themes/lizardtours2/assets/js/main.js
@@ -116,7 +116,7 @@ var $btnMenu = $('#btn-menu'),
           
           $.ajax({
                 type: 'GET',
-                url: '/api/taxonomy/get_taxonomy_posts/?taxonomy=product_cat&slug='+ selectedCategory+'&per_page=-1',//'/api/get_posts/?post_type=tour&count=-1',
+                url: '/api/taxonomy/get_taxonomy_posts/?taxonomy=product_cat&count=-1&slug='+ selectedCategory,//'/api/get_posts/?post_type=tour&count=-1',
                 
                 success: function(data){
                    //console.log(data)
